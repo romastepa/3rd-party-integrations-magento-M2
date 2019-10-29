71fb5ca0b352b11a716bfba732fa309dd54a7d4f
Roman Stepanov <mail2punk@gmail.com>
2019-10-29 16:25:49 +0200

    fixed minor issue

--- Block/Adminhtml/Mapping/Event/Renderer/Emarsyseventmapping.php
+++ Block/Adminhtml/Mapping/Event/Renderer/Emarsyseventmapping.php
@@ -8,16 +8,13 @@
 namespace Emarsys\Emarsys\Block\Adminhtml\Mapping\Event\Renderer;
 
 use Emarsys\Emarsys\Helper\Data as EmarsysHelper;
-use Emarsys\Emarsys\Model\ResourceModel\Customer\Collection;
-use Emarsys\Emarsys\Model\ResourceModel\Emarsysevents\CollectionFactory;
-use Emarsys\Emarsys\Model\ResourceModel\Event;
-use Emarsys\Emarsys\Model\ResourceModel\Sync;
+use Emarsys\Emarsys\Model\ResourceModel\Customer\CollectionFactory as CustomerCollectionFactory;
+use Emarsys\Emarsys\Model\ResourceModel\Emarsysevents\CollectionFactory as EmarsysEventCollectionFactory;
+use Emarsys\Emarsys\Model\ResourceModel\Emarsysmagentoevents\CollectionFactory as MagentoEventsCollectionFactory;
 use Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer;
-use Magento\Backend\Helper\Data;
 use Magento\Backend\Model\Session;
 use Magento\Framework\DataObject;
 use Magento\Framework\UrlInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
 /**
  * Class Emarsyseventmapping
@@ -36,27 +33,7 @@ class Emarsyseventmapping extends AbstractRenderer
     protected $collectionFactory;
 
     /**
-     * @var Data
-     */
-    protected $backendHelper;
-
-    /**
-     * @var Sync
-     */
-    protected $syncResourceModel;
-
-    /**
-     * @var Event
-     */
-    protected $resourceModelEvent;
-
-    /**
-     * @var StoreManagerInterface
-     */
-    protected $_storeManager;
-
-    /**
-     * @var CollectionFactory
+     * @var EmarsysEventCollectionFactory
      */
     protected $emarsysEventCollection;
 
@@ -66,42 +43,39 @@ class Emarsyseventmapping extends AbstractRenderer
     protected $emarsysHelper;
 
     /**
-     * @var \Emarsys\Emarsys\Model\ResourceModel\Emarsysmagentoevents\CollectionFactory
+     * @var MagentoEventsCollectionFactory
      */
-    protected $magentoEventCollection;
+    protected $magentoEventsCollection;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlInterface;
 
     /**
      * Emarsyseventmapping constructor.
+     *
      * @param Session $session
-     * @param \Emarsys\Emarsys\Model\ResourceModel\Customer\CollectionFactory $collectionFactory
-     * @param Data $backendHelper
-     * @param Event $resourceModelEvent
-     * @param StoreManagerInterface $storeManager
+     * @param CustomerCollectionFactory $collectionFactory
      * @param UrlInterface $urlInterface
      * @param EmarsysHelper $emarsysHelper
-     * @param \Emarsys\Emarsys\Model\ResourceModel\Emarsysmagentoevents\CollectionFactory $magentoEventCollection
-     * @param CollectionFactory $emarsysEventCollection
+     * @param MagentoEventsCollectionFactory $magentoEventsCollection
+     * @param EmarsysEventCollectionFactory $emarsysEventCollection
      */
     public function __construct(
         Session $session,
-        \Emarsys\Emarsys\Model\ResourceModel\Customer\CollectionFactory $collectionFactory,
-        Data $backendHelper,
-        Event $resourceModelEvent,
-        StoreManagerInterface $storeManager,
+        CustomerCollectionFactory $collectionFactory,
         UrlInterface $urlInterface,
-        Data $emarsysHelper,
-        \Emarsys\Emarsys\Model\ResourceModel\Emarsysmagentoevents\CollectionFactory $magentoEventCollection,
-        CollectionFactory $emarsysEventCollection
+        EmarsysHelper $emarsysHelper,
+        MagentoEventsCollectionFactory $magentoEventsCollection,
+        EmarsysEventCollectionFactory $emarsysEventCollection
     ) {
         $this->session = $session;
         $this->collectionFactory = $collectionFactory;
-        $this->backendHelper = $backendHelper;
-        $this->resourceModelEvent = $resourceModelEvent;
-        $this->_storeManager = $storeManager;
         $this->emarsysEventCollection = $emarsysEventCollection;
         $this->emarsysHelper = $emarsysHelper;
-        $this->_urlInterface = $urlInterface;
-        $this->magentoEventCollection = $magentoEventCollection;
+        $this->urlInterface = $urlInterface;
+        $this->magentoEventsCollection = $magentoEventsCollection;
     }
 
     /**
@@ -111,17 +85,15 @@ class Emarsyseventmapping extends AbstractRenderer
     public function render(DataObject $row)
     {
         $id = $row->getId();
-        $magentoEventName = $this->magentoEventCollection->create()
+        $magentoEventName = $this->magentoEventsCollection->create()
             ->addFieldToFilter('id', $id)
             ->getFirstItem()
             ->getData('magento_event');
 
-        $emarsysEventname = trim(str_replace(" ", "_", strtolower($magentoEventName)));
-        $session = $this->session;
-        $storeId = $session->getStoreId();
-        $gridSessionData = $session->getMappingGridData();
-        $url = $this->_urlInterface->getUrl('*/*/changeValue');
-        $emarsysEvents = $this->emarsysEventCollection->create()->addFieldToFilter('store_id', ['eq' => $storeId]);
+        $emarsysEventName = trim(str_replace(" ", "_", strtolower($magentoEventName)));
+        $gridSessionData = $this->session->getMappingGridData();
+
+        $emarsysEvents = $this->emarsysEventCollection->create()->addFieldToFilter('store_id', ['eq' => $this->session->getStoreId()]);
         $dbEvents = $emarsysEvents->getAllIds();
 
         $readOnly = '';
@@ -129,16 +101,16 @@ class Emarsyseventmapping extends AbstractRenderer
             $readOnly .= 'disabled = disabled ';
         }
 
-        $html = '<select ' . $readOnly . 'name="directions" style="width:200px;" onchange="changeEmarsysValue(\'' . $url . '\', this.value, \'' . $id . '\', \'' . $id . '\')";>
-			<option value="0">Please Select</option>';
+        $html = '<select ' . $readOnly . 'name="directions" style="width:200px;" onchange="changeEmarsysValue(\'' . $this->urlInterface->getUrl('*/*/changeValue') . '\', this.value, \'' . $id . '\', \'' . $id . '\')";>'
+			. '<option value="0">Please Select</option>';
 
         $gridSessionData[$id]['magento_event_id'] = $id;
         foreach ($emarsysEvents as $emarsysEvent) {
             $sel = '';
 
             if (($row->getEmarsysEventId() == $emarsysEvent->getId())
-                || (($emarsysEventname == $emarsysEvent->getEmarsysEvent()) && ($row->getEmarsysEventId() == null))
-                || (($emarsysEventname == $emarsysEvent->getEmarsysEvent()) && ($row->getEmarsysEventId() != null) && !in_array($row->getEmarsysEventId(), $dbEvents))
+                || (($emarsysEventName == $emarsysEvent->getEmarsysEvent()) && ($row->getEmarsysEventId() == null))
+                || (($emarsysEventName == $emarsysEvent->getEmarsysEvent()) && ($row->getEmarsysEventId() != null) && !in_array($row->getEmarsysEventId(), $dbEvents))
             ) {
                 $sel .= 'selected = selected';
                 $gridSessionData[$id]['emarsys_event_id'] = $emarsysEvent->getId();
@@ -147,8 +119,8 @@ class Emarsyseventmapping extends AbstractRenderer
             $html .= '<option ' . $sel . ' value="' . $emarsysEvent->getId() . '">' . $emarsysEvent->getEmarsysEvent() . '</option>';
         }
         $html .= '</select>';
-        $session->setStoreId($storeId);
-        $session->setMappingGridData($gridSessionData);
+
+        $this->session->setMappingGridData($gridSessionData);
         return $html;
     }
 }
