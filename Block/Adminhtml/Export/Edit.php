5d998b23ae15f8cbb16ea45c0e34fac76ab7c219
Roman Stepanov <mail2punk@gmail.com>
2019-11-05 21:10:01 +0200

    added missing file

---
+++ Block/Adminhtml/Export/Edit.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @category   Emarsys
+ * @package    Emarsys_Emarsys
+ * @copyright  Copyright (c) 2017 Emarsys. (http://www.emarsys.net/)
+ */
+
+namespace Emarsys\Emarsys\Block\Adminhtml\Export;
+
+use Magento\Backend\Block\Widget\Context;
+use Magento\Backend\Block\Widget\Form\Container;
+use Emarsys\Emarsys\Helper\Data as EmarsysHelper;
+use Magento\Framework\App\Request\Http;
+
+/**
+ * Class Edit
+ *
+ * @package Emarsys\Emarsys\Block\Adminhtml\Export
+ */
+class Edit extends Container
+{
+    protected $_objectId = 'entity_id';
+    protected $_blockGroup = 'Emarsys_Emarsys';
+
+    /**
+     * @var EmarsysHelper
+     */
+    protected $emarsysHelper;
+
+    /**
+     * @var Http
+     */
+    protected $getRequest;
+
+    /**
+     * Edit constructor.
+     * @param Context $context
+     * @param Http $request
+     * @param EmarsysHelper $emarsysHelper
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Http $request,
+        EmarsysHelper $emarsysHelper,
+        array $data = []
+    ) {
+        $this->getRequest = $request;
+        $this->emarsysHelper = $emarsysHelper;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Check permission for passed action
+     *
+     * @param string $resourceId
+     * @return bool
+     */
+    protected function _isAllowedAction($resourceId)
+    {
+        return $this->_authorization->isAllowed($resourceId);
+    }
+}
