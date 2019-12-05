6d07b083d6a453dc3f70f6a87019a3a0e3a2a70f
Roman Stepanov <mail2punk@gmail.com>
2019-12-05 19:00:36 +0200

    added missed file set-shipping-information-mixin.js

---
+++ view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -0,0 +1,27 @@
+/**
+ * @category   Emarsys
+ * @package    Emarsys_Emarsys
+ * @copyright  Copyright (c) 2017 Emarsys. (http://www.emarsys.net/)
+ */
+/*global define,alert*/
+define([
+    'jquery',
+    'mage/utils/wrapper',
+    'Magento_Checkout/js/model/quote'
+], function ($, wrapper, quote) {
+    'use strict';
+
+    return function (setShippingInformationAction) {
+
+        return wrapper.wrap(setShippingInformationAction, function (originalAction) {
+            var shippingAddress = quote.shippingAddress();
+            if (shippingAddress['extension_attributes'] === undefined) {
+                shippingAddress['extension_attributes'] = {};
+            }
+
+            shippingAddress['extension_attributes']['emarsys_subscriber'] = (jQuery('#emarsys_subscriber').is(':checked') ? 1 : 0);
+            // pass execution to original action ('Magento_Checkout/js/action/set-shipping-information')
+            return originalAction();
+        });
+    };
+});
