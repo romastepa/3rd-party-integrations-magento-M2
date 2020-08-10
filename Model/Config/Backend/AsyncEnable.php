e44672e41dfe37cdd7a2922bcaf6626a11b0aab8
Roman Stepanov <mail2punk@gmail.com>
2020-08-10 15:09:41 +0300

    ## v1.0.30 - small fixes

---
+++ Model/Config/Backend/AsyncEnable.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @category  Emarsys
+ * @package   Emarsys_Emarsys
+ * @copyright Copyright (c) 2020 Emarsys. (http://www.emarsys.net/)
+ */
+
+namespace Emarsys\Emarsys\Model\Config\Backend;
+
+use Exception;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\App\Config\ValueFactory;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Registry;
+
+class AsyncEnable extends Value
+{
+    /**
+     * Cron string path
+     */
+    const CRON_STRING_PATH = 'emartech/async/cron_expr';
+
+    /**
+     * @var ValueFactory
+     */
+    protected $_configValueFactory;
+
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param ScopeConfigInterface $config
+     * @param TypeListInterface $cacheTypeList
+     * @param ValueFactory $configValueFactory
+     * @param AbstractResource $resource
+     * @param AbstractDb $resourceCollection
+     * @param string $runModelPath
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ScopeConfigInterface $config,
+        TypeListInterface $cacheTypeList,
+        ValueFactory $configValueFactory,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        $runModelPath = '',
+        array $data = []
+    ) {
+        $this->_configValueFactory = $configValueFactory;
+        parent::__construct($context, $registry, $config, $cacheTypeList, $resource, $resourceCollection, $data);
+    }
+
+    /**
+     * Processing object after save data
+     *
+     * @return $this
+     * @throws Exception
+     */
+    public function afterSave()
+    {
+        $enable = $this->getValue();
+
+        try {
+            if (!$enable) {
+                $cronExprString = '';
+                $this->_configValueFactory->create()->load(self::CRON_STRING_PATH, 'path')
+                    ->setValue($cronExprString)
+                    ->setPath(self::CRON_STRING_PATH)
+                    ->setScope($this->getScope())
+                    ->setScopeId($this->getScopeId())
+                    ->save();
+            }
+        } catch (Exception $e) {
+            throw new Exception(__('We can\'t save the cron expression.'));
+        }
+
+        return parent::afterSave();
+    }
+}
